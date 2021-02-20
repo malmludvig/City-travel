9a5aa8e128041e2e0ede5e928a79182d24a5d2a5
Ludvig <malmludvig@gmail.com>
2021-02-21 00:41:02 +0100

    Added fetch functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-  }
+
+//https://api.foursquare.com/v2/venues/search?near=göteborg&client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&v=20210220
+
+fetch('https://api.openweathermap.org/data/2.5/weather?id=2643743&appid=725df4167c36c97949a6fa606140db86')
+.then(res => res.json())
+.then(function(data){
+
+  console.log(data)
+  drawWeather(data)
+  //console.log(celcius + ' degrees in ' + data.name);
+
+}
+)
+
+function drawWeather(data) {
+var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+document.getElementById('weatherTag').innerHTML = data.name + "<br>" + celcius + " &deg";
+
+
+}
