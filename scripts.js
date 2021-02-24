a398175256e6e09905a47442ba53fbee3b6184bd
Ludvig <malmludvig@gmail.com>
2021-02-24 19:16:36 +0100

    Added asynchronous search functionality for both APIs

--- scripts.js
+++ scripts.js
@@ -1,37 +1,47 @@
+const searchButton = document.querySelector(".submit");
+searchButton.addEventListener('click', function(){
 
+  let searchInput = document.querySelector(".input_text").value;
+console.log(searchInput);
 
-fetch('https://api.openweathermap.org/data/2.5/weather?id=2711537&appid=725df4167c36c97949a6fa606140db86')
-.then(res => res.json())
-.then(function(data){
+const url1 = 'https://api.openweathermap.org/data/2.5/weather?q='+searchInput+'&appid=725df4167c36c97949a6fa606140db86'
 
-  console.log(data)
-  drawWeather(data)
-  //console.log(celcius + ' degrees in ' + data.name);
-
+  fetch(url1)
+  .then(res => res.json())
+  .then(function(data){
+  
+    console.log(data)
+    drawWeather(data)
+    //console.log(celcius + ' degrees in ' + data.name);
+  
+    }
+  )
+  
+  function drawWeather(data) {
+  var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+  document.getElementById('weatherTag').innerHTML = data.name + "<br>" + celcius + " &deg";
+  
   }
-)
 
-function drawWeather(data) {
-var celcius = Math.round(parseFloat(data.main.temp)-273.15);
-document.getElementById('weatherTag').innerHTML = data.name + "<br>" + celcius + " &deg";
+  const url2 = 'https://api.foursquare.com/v2/venues/search?near='+searchInput+'&client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&v=20210220'
 
-}
 
-fetch('https://api.foursquare.com/v2/venues/search?near=göteborg&client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&v=20210220')
-.then(res => res.json())
-.then(function(data){
-
-  console.log(data)
-  drawVenues(data)
-  //console.log(celcius + ' degrees in ' + data.name);
-
+  fetch(url2)
+  .then(res => res.json())
+  .then(function(data){
+  
+    console.log(data)
+    drawVenues(data)
+  
+    }
+  )
+  
+  function drawVenues(data) {
+  document.getElementById('venueTag1').innerHTML = data.response.venues[2].name+ "";
+  document.getElementById('venueTag2').innerHTML = data.response.venues[3].name+ "";
+  document.getElementById('venueTag3').innerHTML = data.response.venues[4].name+ "";
+  
   }
-)
 
-function drawVenues(data) {
-document.getElementById('venueTag1').innerHTML = data.response.venues[2].name+ "";
-document.getElementById('venueTag2').innerHTML = data.response.venues[3].name+ "";
-document.getElementById('venueTag3').innerHTML = data.response.venues[4].name+ "";
+});
 
-
-}
