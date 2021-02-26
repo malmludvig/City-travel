e8df9cbd59c8321e310aa08eee9a55c560aecabe
Ludvig <malmludvig@gmail.com>
2021-02-26 13:54:40 +0100

    Added array of venue objects

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 const searchButton = document.querySelector(".submit");
 
-let sortCheck = true;
-
 function drawWeather(data) {
   var celcius = Math.round(parseFloat(data.main.temp)-273.15);
   let iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
   document.getElementById('weatherTag').innerHTML = "<img src='"+iconUrl+"'<br><br>"+data.name + "<br>" + celcius + " &deg";
-  
+
   }
 
   function weatherFunction(searchInput) {
@@ -18,32 +16,41 @@ function drawWeather(data) {
       .then(res => res.json())
       .then(function(data){
       
-        drawWeather(data)
         console.log(data)
-      
+        drawWeather(data)
+
         }
       )
     }
 
     function drawVenues(data) {
-      document.getElementById('venueTag1').innerHTML = data.response.venues[0].name+ "";
-      document.getElementById('venueTag2').innerHTML = data.response.venues[1].name+ "";
-      document.getElementById('venueTag3').innerHTML = data.response.venues[2].name+ "";
 
-      
+      //let venuesArray = [data.response.venues[0].name, data.response.venues[1].name, data.response.venues[2].name];
+
+      let venuesArray = [{name: data.response.venues[0].name, address: data.response.venues[0].location.address}, {name: data.response.venues[1].name, address: data.response.venues[1].location.address}, {name: data.response.venues[2].name, address: data.response.venues[2].location.address}]
+
+
       if (document.getElementById('sortCheck').checked){
-        let venuesArray = [data.response.venues[0].name, data.response.venues[1].name, data.response.venues[2].name];
+        
         console.log(venuesArray)
-        venuesArray.sort();
+
+        //venuesArray.sort();
+
+        venuesArray = _.sortBy( venuesArray, 'name' );
+
         console.log(venuesArray)
 
       }
-    
+
+      document.getElementById('venueTag1').innerHTML = venuesArray[0].name+ " <br>" + venuesArray[0].address;
+      document.getElementById('venueTag2').innerHTML = venuesArray[1].name+ " <br>" + venuesArray[1].address; 
+      document.getElementById('venueTag3').innerHTML = venuesArray[2].name+ " <br>" + venuesArray[2].address
+
       }
 
     function venuesFunction(searchInput) {
 
-      const url2 = 'https://api.foursquare.com/v2/venues/search?near='+searchInput+'&limit=10&client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&v=20210220'
+      const url2 = 'https://api.foursquare.com/v2/venues/search?near='+searchInput+'&limit=10&client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&v=20210226'
     
       fetch(url2)
       .then(res => res.json())
