d0a4f4c545bd8f9af8ccc49bc6b99bfb044ec9cd
Ludvig <malmludvig@gmail.com>
2021-03-03 09:14:51 +0100

    Additional final touches

--- scripts.js
+++ scripts.js
@@ -13,24 +13,35 @@ function sortAlphabetically(a, b) {
 	return 0;
 }
 
+function dateOfToday() {
+
+	let todaysDate = new Date();
+	let dd = String(todaysDate.getDate()).padStart(2, '0');
+	let mm = String(todaysDate.getMonth() + 1).padStart(2, '0');
+	let yyyy = todaysDate.getFullYear();
+	todaysDate = yyyy + mm + dd;
+	return todaysDate
+}
+
 function drawWeather(data) {
 	var celcius = Math.round(parseFloat(data.main.temp) - 273.15);
 	let iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-	document.getElementById('weatherTag').innerHTML = "<img src='" + iconUrl + "'<br><br>" + data.name + "<br>" + celcius + " °";
+	document.getElementById('weatherTag').innerHTML = "<img src='" + iconUrl + "'<br><br>" + data.name + "<br>" + celcius + " °<br><span class='weatherDescriptionFont'>Condition: "+data.weather[0].description+"</span>";
 
 }
 
 function weatherFunction(searchInput) {
 
-	const url1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&appid=725df4167c36c97949a6fa606140db86'
+	let apiKey = "725df4167c36c97949a6fa606140db86"
+	const apiURLWeather = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}`
 
-	fetch(url1)
+	fetch(apiURLWeather)
 		.then(res => res.json())
 		.then(function (data) {
 			try {
 				drawWeather(data)
 			} catch (err) {
-				alert("Error, something went wrong! See the console for more info.")
+				alert("Error, something went wrong with the Openweather API! See the console for more info.")
 				console.log(err)
 			}
 		})
@@ -64,17 +75,20 @@ function drawVenues(data) {
 
 function venuesFunction(searchInput) {
 
-	const url2 = `https://api.foursquare.com/v2/venues/search?client_id=MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR&client_secret=YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW&near=${searchInput}&limit=6&v=20210226`
+	let todaysDate = dateOfToday()
+			   
+	let apiKey = "MWUVZLCCQRKDNGXZRGGLIUVBW04U0ETEPAQPJZBE2X3QV2MR"
+	let apiSecret = "YF4IVTB1ZNP03GHZ2LB5XUIHRERLNYMFW32BNOSEOCZWZJTW"
+	const apiURLVenues = `https://api.foursquare.com/v2/venues/search?client_id=${apiKey}&client_secret=${apiSecret}&near=${searchInput}&limit=6&v=${todaysDate}`
 
-	fetch(url2)
+	fetch(apiURLVenues)
 		.then(res => res.json())
 		.then(function (data) {
 
-			console.log(data)
 			try {
 				drawVenues(data)
 			} catch (err) {
-				alert("Error, something went wrong! See the console for more info.")
+				alert("Error, something went wrong with the FourSquare API! See the console for more info.")
 				console.log(err)
 			}
 		})
@@ -91,8 +105,7 @@ searchButton.addEventListener('click', function () {
 
 	} else if (document.getElementById('onlyWeather').checked) {
 
-
-		for (i = 0; i < 5; i++) {
+		for (i = 0; i < 6; i++) {
 
 			document.getElementById(`venueTag${i}`).innerHTML = "";
 		}
